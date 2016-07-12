e3144a8a5b87e0077e4c2745e00a6521bd5aacd5
Ben Sapiro <ben@sapiro.net>
2016-07-11 22:59:59 -0400

    matrix is still not working

--- _.py
+++ _.py
@@ -1,47 +1,18 @@
-x = [1,2,3,4,5, [[61,7,8,9],
-                 [62,7,8,9],
-                 [63,7,8,9, 0, 0]
-                 ]]
+from gtool.core.types.matrix import Matrix
 
-y = [1,2,3,4,5,6,7]
-z = [1,2,3,4,5, [6,7,8]]
+m = Matrix(startwidth=4, startheight=4)
 
-a = [1, 2, 3, 4, 5, [[61, 7, 8, 9],
-                     [62, 7, 8, 9],
-                     [63, 7, 8, 9, 0, [
-                         [1,2,3],
-                         [1,2,3],
-                         [1,2,3]
-                     ]]
-                     ]]
-def looper(iteritem, currentheight=1):
-    height = currentheight
-    width = 0
+for x in m.data():
+    print(x)
 
-    print('len:',len(iteritem))
-    if all(isinstance(f, list) for f in iteritem):
-        height += len(iteritem)
-        width += max([len(f) for f in iteritem])
-    for i in iteritem:
-        if not isinstance(i, list):
-            width += 1
-        elif isinstance(i, list):
-            _tup = looper(i, currentheight=height)
-            height = _tup[0]
-            width += _tup[1]
+print(m.vertical_utilization())
 
-    #height = max(1,height)
-    return (height,width)
+print(m.horitzontal_utlization())
 
-def recursive_len(item):
-    if type(item) == list:
-        return sum(recursive_len(subitem) for subitem in item)
-    else:
-        return 1
+print(m.cursor)
 
-#print(looper(x))
-#print(looper(y))
-#print(looper(z))
-print(looper(a))
-print(recursive_len(a))
+m.insert(cursor=(0,2), datamatrix=[1,2,3,4,5,6])
+
+for x in m.data():
+    print(x)
 
--- gtool/core/types/matrix.py
+++ gtool/core/types/matrix.py
@@ -1,15 +1,15 @@
 class Matrix(object):
 
-    def __init__(self, startwidth=100, startheight=50):
+    def __init__(self, startwidth=100, startheight=100):
         self.__storage__ = [[None for i in range(startwidth)] for i in range(startheight)]
         self.__current_row__ = 0
         self.__current_col__ = 0
         self.__utilization_threshold__ = .75
 
-
     @property
     def cursor(self):
-        return (self.__current_row__, self.__current_col__)
+        # x,y position
+        return (self.__current_col__, self.__current_row__)
 
     @property
     def currentrow(self):
@@ -19,48 +19,133 @@ class Matrix(object):
     def currentcol(self):
         return self.__current_col__
 
+    def __height__(self):
+        return len(self.__storage__)
+
+    @property
+    def height(self):
+        return self.__height__()
+
+    def __width__(self):
+        #assumes matrix is uniformly sized
+        return len(self.__storage__[0])
+
+    @property
+    def width(self):
+        return self.__width__()
+
+    def dimensions(self):
+        return (self.__width__(), self.__height__())
+
     def horitzontal_utlization(self):
-        pass
+        _result = 0
+        rowlength = self.__width__()
+        for row in self.__storage__:
+            endoflist = 0
+            for i, cell in enumerate(row):
+                x = i+1
+                if row[-x] is not None:
+                    endoflist = rowlength - x
+                    break # avoids unnecesary interations
+            _result = max(endoflist, _result)
+        # calculate utlization
+        return round(_result/rowlength, 2)
 
     def vertical_utilization(self):
-        pass
+        colheight = len(self.__storage__)
+        for i, row in enumerate(self.__storage__):
+            if any(x is not None for x in self.__storage__[-i+1]):
+                return round((colheight - (i+1))/colheight, 2)
 
-    def expand_vertical_space(self):
-        pass
+    def utilization(self):
+        # x,y
+        return(self.horitzontal_utlization(),self.vertical_utilization())
 
-    def insert_vertical_space(self):
-        pass
+    def append_cols(self, cols=10):
+        rowlength = self.__width__()
+        for i, row in enumerate(self.__storage__):
+            if len(row) == rowlength:
+                row.extend([None for x in range(cols)])
+            else:
+                raise IndexError('Row %s is not the same length as the other rows in the matrix' % i)
+        return True
 
-    def append_vertical_space(self):
-        pass
+    def insert_cols(self, x, cols=10):
+        # insert col at x
+        if cols < 1:
+            raise ValueError('cannot insert less than 1 column.')
+        if x < 0 or x >= self.__width__():
+            raise IndexError('cannot insert cols outside the x range of the matrix. Use append instead')
+        try:
+            for row in self.__storage__:
+                row[x:x] = [None] * cols
+        except:
+            raise Exception('could not insert %s columns at column %s in the matrix' % (cols, x))
 
-    def expand_horizontal_space(self):
-        pass
+        return True
 
-    def insert_horizontal_space(self):
-        pass
+    def insert_rows(self, y, rows=10):
+        #insert row at y
+        if rows < 1:
+            raise ValueError('cannot insert less than 1 row.')
+        if y < 0 or y >= self.__height__():
+            raise IndexError('cannot insert rows outside the y range of the matrix. Use append instead')
+        try:
+            self.__storage__[y:y] = [[None] * self.__width__()] * rows
+            #.insert(y, [[None for j in range(self.width)] for i in range(rows)]):
+        except:
+            raise Exception('could not insert %s rows at row %s in the matrix' % (rows, y))
 
-    def append_horizontal_space(self):
-        pass
+        return True
 
-    def update_row(self):
-        pass
 
-    def append_row(self):
-        pass
+    def append_rows(self, rows=10):
+        rowlength = self.__width__()
+        _newrows = [[None for x in range(rowlength)] for y in range(rows)]
+        self.__storage__.extend(_newrows)
+        return True
 
-    def update_col(self):
-        pass
+    def getrow(self, y):
+        return self.__storage__[y]
 
-    def append_col(self):
-        pass
+    def getcol(self, x):
+        return [row[x] for row in self.__storage__]
+
+    def getcell(self, x, y):
+        return self.__storage__[y][x]
 
     def data(self):
-        #iter generator
-        pass
+        # return storage one row at a time iter generator
+        return (row for row in self.__storage__)
 
-    def append_cells_at_cursor(self):
-        pass
+    def insert(self, cursor=(0,0), datamatrix=None):
+        x, y = cursor
+        if datamatrix is None or not isinstance(datamatrix, list):
+            raise ValueError('Was expecting to insert a list but got a %s' % type(datamatrix))
+        if x < 0:
+            raise IndexError('A coordinate outside the x axis of the matrix was provided')
+        if y < 0:
+            raise IndexError('A coordinate outside the y axis of the matrix was provided')
 
-    def append_row_below_cursor(self):
-        pass
+        if y > int(self.__height__() * self.__utilization_threshold__):
+            # grow enough so that we're back to ~70% vertical utilization
+            _delta = int(y / (self.__utilization_threshold__ - .05) - self.__height__()) + 1
+            print(_delta)
+            if not self.append_rows(rows=_delta):
+                raise Exception('Could not append more rows to the matrix')
+
+        if x + len(datamatrix) > self.__width__():
+            print('expanding x')
+            _delta = int(x / (self.__utilization_threshold__ - .05) - self.__width__()) + 1
+            print(_delta)
+            if not self.append_cols(cols=_delta):
+                raise Exception('Could not append more columns to the matrix')
+
+        # TODO secondary safety check to make sure we'll be in range
+
+        _x_end = x + (len(datamatrix) - 1)
+        self.__storage__[y][x:_x_end] = datamatrix
+
+    def __matrixmap__(self):
+        # returns a matrix of the same size but shows how much data is
+        return [[len(x) if x is not None else 0 for x in y] for y in self.__storage__]
