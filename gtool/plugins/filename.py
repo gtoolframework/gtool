67c08bec87c0ab41a8001f6fe58cbe6cdf8063d8
Ben Sapiro <ben@sapiro.net>
2016-09-02 22:45:27 -0400

    filename method plugin implemented

---
+++ gtool/plugins/filename.py
@@ -0,0 +1,23 @@
+from gtool.core.types.core import FunctionType
+#import codecs
+import os.path as path
+
+class Filename(FunctionType):
+
+    def __init__(self, obj, config=None):
+
+        if config is not None:
+            raise ValueError('Filename method plugin does not accept a configuration')
+
+        super(Filename, self).__init__(obj, config=None)
+
+        self.computable = True
+
+    def compute(self):
+        if self.computable:
+            #_result = codecs.decode(self.context['file'], 'unicode_escape')
+            _result = path.split(self.context['file'])[1]
+            self.__result__ = _result
+
+def load():
+    return Filename
