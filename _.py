9b9fcb4afca4b5db6e6058960c0059ea5a47ced6
Ben Sapiro <ben@sapiro.net>
2016-10-15 23:05:45 -0400

    tweaking setup packaging

--- _.py
+++ _.py
@@ -1,18 +1,3 @@
-import pyparsing as p
+from setuptools import setup, find_packages
 
-config = '@blah[3:7]'
-
-attrexpr = (p.Literal('@') | p.Literal('!')).suppress() + p.Word(p.alphanums)
-slicestartexpr = p.Literal('[').suppress() + p.Optional(p.Word(p.nums))
-sliceendexpr =  p.Optional(p.Word(p.nums)) + p.Literal(']').suppress()
-
-attrmatch = attrexpr.setResultsName('attribute') + \
-            slicestartexpr.setResultsName('start') + \
-            p.Suppress(p.Literal(':')) + \
-            sliceendexpr.setResultsName('end')
-
-s = attrmatch.parseString(config)
-
-print(s.get('attribute', None))
-print(s.start)
-print(s.end)
+print(find_packages())
